f6244a6d9e14a761edb10827943384e8ab47aa74
kwishnu <kwish1777@gmail.com>
2020-04-04 17:35:53 -0700

    Add radiobuttons to AddItemActivity

--- app/src/main/java/com/baked/inscriptainventory/AddItemActivity.kt
+++ app/src/main/java/com/baked/inscriptainventory/AddItemActivity.kt
@@ -1,6 +1,7 @@
 package com.baked.inscriptainventory
 
 import android.os.Bundle
+import android.widget.RadioButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_item.*
@@ -14,8 +15,16 @@ class AddItemActivity : AppCompatActivity() {
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_chevron_left_white_18dp)
         android.app.ActionBar.DISPLAY_HOME_AS_UP
 
-
-
+        var currentSelected = radio0
+        listOf<RadioButton>(
+            radio0, radio1, radio2, radio3, radio4, radio5, radio6, radio7
+        ).forEach {
+            it.setOnClickListener { _ ->
+                currentSelected.isChecked = false
+                currentSelected = it
+                currentSelected.isChecked = true
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
