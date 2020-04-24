7db009b8b928bd5effa449aaaa3da06940554496
kwishnu <kwish1777@gmail.com>
2020-04-24 09:36:55 -0700

    Use server images with RecyclerView

--- app/src/main/java/com/baked/inscriptainventory/Resource/ImagesArray.kt
+++ app/src/main/java/com/baked/inscriptainventory/Resource/ImagesArray.kt
@@ -1,6 +1,6 @@
 package com.baked.inscriptainventory.Resource
 
-class ImagesArray(
+class ImagesArray {
     val IMAGE_URI: Array<String> = arrayOf(
         "blank",
         "strip_tube_1",
@@ -11,4 +11,23 @@ class ImagesArray(
         "rotary_growth_vial",
         "bottle"
     )
-)
+
+    val imageList: ArrayList<String> = arrayListOf(
+        "file:///android_asset/blank.png",
+        "file:///android_asset/strip_tube_1.png",
+        "file:///android_asset/strip_tube_4.png",
+        "file:///android_asset/strip_tube_6.png",
+        "file:///android_asset/tubes_w_caps.png",
+        "file:///android_asset/cell_tube.png",
+        "file:///android_asset/rotary_growth_vial.png",
+        "file:///android_asset/bottle.png"
+
+//    "https://i.imgur.com/3PHk3OW.png",
+//    "https://i.imgur.com/ww64v2S.png",
+//    "https://i.imgur.com/PLM10dh.png",
+//    "https://i.imgur.com/0YpzD1i.png",
+//    "https://i.imgur.com/rf2zKPV.png",
+//    "https://i.imgur.com/0GQdCRy.png",
+//    "https://i.imgur.com/AgttLj2.png"
+    )
+}
