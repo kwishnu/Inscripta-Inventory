9243a65e52b28c84bbfa6891fa4e6f8b6c794b93
kwishnu <kwish1777@gmail.com>
2020-05-03 15:02:34 -0700

    Added fragments

--- app/src/main/java/com/baked/inscriptainventory/Adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/com/baked/inscriptainventory/Adapter/SectionsPagerAdapter.kt
@@ -27,8 +27,21 @@ class SectionsPagerAdapter(
             8 -> Fragment8(invItems[position], images)
             9 -> Fragment9(invItems[position], images)
             10 -> Fragment10(invItems[position], images)
+            11 -> Fragment11(invItems[position], images)
+            12 -> Fragment12(invItems[position], images)
+            13 -> Fragment13(invItems[position], images)
+            14 -> Fragment14(invItems[position], images)
+            15 -> Fragment15(invItems[position], images)
+            16 -> Fragment16(invItems[position], images)
+            17 -> Fragment17(invItems[position], images)
+            18 -> Fragment18(invItems[position], images)
+            19 -> Fragment19(invItems[position], images)
+            20 -> Fragment20(invItems[position], images)
+            21 -> Fragment21(invItems[position], images)
+            22 -> Fragment22(invItems[position], images)
+            23 -> Fragment23(invItems[position], images)
 
-            else -> Fragment11(invItems[position], images)
+            else -> Fragment24(invItems[position], images)
         }
     }
 
